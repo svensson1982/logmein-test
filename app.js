6d3e0bf81cbf4ab0ffa2c5ceae6de4beeeac13cf
svensson1982 <svensson1982@users.noreply.github.com>
2017-09-18 09:12:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var bookApp = angular.module('bookApp', ['ngResource', 'ngRoute', 'ngStorage']);
 bookApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', {templateUrl: 'templates/book-list.html', controller: 'BookListCtrl'})
-        .when('/details', {templateUrl: 'templates/details.html', controller: 'BookDetailsCtrl'})
-        .when('/cart', {templateUrl: 'templates/cart.html', controller: 'cartCtrl'});
+        .when('/details', {templateUrl: 'templates/details.html', controller: 'BookDetailsCtrl'});
     // use the HTML5 History API
     $locationProvider.hashPrefix('');
     $locationProvider.html5Mode(true);
